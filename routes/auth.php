87683712a05fa0e904f21ac974d23d74075d33cb
Copy Code ID <copycodeid2296@gmail.com>
2024-07-11 13:45:08 +0800

    Registering Login endpoint to auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -4,3 +4,5 @@ use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', Controllers\Auth\RegisterController::class);
+
+Route::post('login', Controllers\Auth\LoginController::class);
