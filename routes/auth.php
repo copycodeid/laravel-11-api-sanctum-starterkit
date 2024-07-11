605d107562738940674f2dd4e3632dcc1e56aa64
Copy Code ID <copycodeid2296@gmail.com>
2024-07-11 14:19:38 +0800

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -8,3 +8,5 @@ Route::post('register', Controllers\Auth\RegisterController::class);
 Route::post('login', Controllers\Auth\LoginController::class);
 
 Route::post('logout', Controllers\Auth\LogoutController::class)->middleware('auth:sanctum');
+
+Route::get('user', Controllers\Auth\AuthenticatedUserController::class)->middleware('auth:sanctum');
