0223fa97a042e81ef526d20b388ccf5ffe7e1358
Copy Code ID <copycodeid2296@gmail.com>
2024-07-13 09:21:45 +0800

    Add Feature Check User Token

--- app/Http/Controllers/Auth/CheckUserTokenController.php
+++ app/Http/Controllers/Auth/CheckUserTokenController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use F9Web\ApiResponseHelpers;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class CheckUserTokenController extends Controller
 {
-    public function __invoke(Request $request)
+    use ApiResponseHelpers;
+
+    public function __invoke(Request $request): JsonResponse
     {
-        //
+        if ($request->user('sanctum')) {
+            return $this->apiResponse([
+                'status' => Response::HTTP_OK,
+                'message' => 'authenticated',
+            ], Response::HTTP_OK);
+        }
+
+        return $this->apiResponse([
+            'status' => Response::HTTP_UNAUTHORIZED,
+            'message' => 'unauthenticated',
+        ], Response::HTTP_UNAUTHORIZED);
     }
 }
--- routes/auth.php
+++ routes/auth.php
@@ -7,6 +7,8 @@ Route::post('register', Controllers\Auth\RegisterController::class);
 
 Route::post('login', Controllers\Auth\LoginController::class);
 
-Route::post('logout', Controllers\Auth\LogoutController::class)->middleware('auth:sanctum');
-
 Route::get('user', Controllers\Auth\AuthenticatedUserController::class)->middleware('auth:sanctum');
+
+Route::get('check-user-token', Controllers\Auth\CheckUserTokenController::class);
+
+Route::post('logout', Controllers\Auth\LogoutController::class)->middleware('auth:sanctum');
